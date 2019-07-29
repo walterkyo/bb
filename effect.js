2c5de54b8a145feaedd9307c95dc95c07df28bf1
Walter Lam <walter_lam@cathaypacific.com>
2019-07-29 17:18:16 +0800

    Added jam dance

--- effect.js
+++ effect.js
@@ -117,6 +117,7 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+			$('#jam').stop();
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
 			$('#b33').animate({top:240, left: vw-150},500);
@@ -124,6 +125,8 @@ $('document').ready(function(){
 			$('#b55').animate({top:240, left: vw+50},500);
 			$('#b66').animate({top:240, left: vw+150},500);
 			$('#b77').animate({top:240, left: vw+250},500);
+
+			$('#jam').animate({bottom:270, left: vw-350},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -139,6 +142,35 @@ $('document').ready(function(){
 			$('#play').fadeIn('slow');
 		});
 	});
+
+	var dancingPose = true;
+
+	function loopJam() {
+		if (dancingPose) {
+			dancingPose = false;
+			$('#jam').css("background-image", "url(dance.png)");
+		} else {
+			dancingPose = true;
+			$('#jam').css("background-image", "url(dance2.png)");
+		}
+		var randleft = 1000*Math.random();
+		var randtop = 270;
+
+		$('#jam').animate({left:randleft,bottom:randtop},1000,function(){
+			loopJam();
+		});
+	}
+
+	function loopJamDance() {
+		$('#jam').css("background-image", "url(dance.png)");
+		$('#jam').fadeIn('slow').delay(500).promise().done(function(){
+			$('#jam').css("background-image", "url(dance2.png)");
+    		$('#jam').fadeIn('slow').delay(500).promise().done(function(){
+    			loopJamDance();
+    		});
+		});
+	}
+
 	$('#play').click(function(){
 		var audio = $('.song')[0];
         audio.play();
@@ -150,10 +182,14 @@ $('document').ready(function(){
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
 		$('body').css('backgroud-color','#FFF');
 		$('body').addClass('peach-after');
+		$('#jam').fadeIn('slow');
+		$('#jam').css("background-image", "url(dance2.png)");
 
 		//play jam photos
 		$(this).fadeOut('slow').delay(0).promise().done(function(){
 			$('#bannar_coming').fadeIn('slow');
+			// loopJamDance();
+			loopJam();
 		});
 	});
 
@@ -241,10 +277,14 @@ $('document').ready(function(){
 		$('.cake').fadeIn('slow');
 		$(this).fadeOut('slow').delay(0).promise().done(function(){
 			$('#light_candle').fadeIn('slow');
+
 		});
 	});
 
 	$('#light_candle').click(function(){
+		$('#jam').stop();
+		$('#jam').fadeOut('slow');
+
 		$(this).fadeOut('slow').promise().done(function(){
 			$('#lighter_img').attr('src', 'firelighter.png').load(function(){
     			this.width;
